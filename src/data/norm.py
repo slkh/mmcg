320236256d459a4c4aeee4a37a3adaca0cfdda4c
Salam Khalifa <salam.khalifa@gmail.com>
2024-05-27 17:40:45 -0400

    I dont know pandas

--- src/data/norm.py
+++ src/data/norm.py
@@ -52,8 +52,10 @@ def load(class_type: int, mode: str) -> datasets.Dataset:
     # {"file_id": "O01008ZE1", "segment_id": "O01008ZE1_0001", "segment_overlap_pct": 0.1449333333333319, "start": 2396.353, "end": 2398.774, "text": "И весь современный мир — это сейчас микс.", "speaker": "SPEAKER_01", "transcript_overlap_pct": 0.8979760429575127, "NORM": {"APOLOGY_ADHERENCES": 0, "APOLOGY_VIOLATIONS": 0, "CRITICISM_ADHERENCES": 0, "CRITICISM_VIOLATIONS": 0, "GREETING_ADHERENCES": 0, "GREETING_VIOLATIONS": 0, "REQUEST_ADHERENCES": 0, "REQUEST_VIOLATIONS": 0, "PERSUASION_ADHERENCES": 0, "PERSUASION_VIOLATIONS": 0, "THANKING_ADHERENCES": 0, "THANKING_VIOLATIONS": 0, "LEAVING_ADHERENCES": 0, "LEAVING_VIOLATIONS": 0, "ADMIRATION_ADHERENCES": 0, "ADMIRATION_VIOLATIONS": 0, "FINALIZE_DEAL_ADHERENCES": 0, "FINALIZE_DEAL_VIOLATIONS": 0, "REFUSE_REQUEST_ADHERENCES": 0, "REFUSE_REQUEST_VIOLATIONS": 0}, "EMOTION": {"ANGER": 0, "ANTICIPATION": 0, "DISGUST": 0, "FEAR": 0, "JOY": 0, "SADNESS": 0, "SURPRISE": 0, "TRUST": 0, "MULTI_SPEAKER": "False"}, "VA": {"VALENCE_AVG": 543.0, "AROUSAL_AVG": 578.3333333333334, "VALENCE_BINNED_AVG": 3.0, "AROUSAL_BINNED_AVG": 3.333333333333333, "VALENCE_AVG_Z": 0.0240707300729762, "AROUSAL_AVG_Z": 0.1260399460227707}, "catalog_id": "LDC2024E15", "duration": 2.4209999999998217, "url": "https://vk.com/video-76218259_456239289"}
     # drop everything except the text and the norms
     df = df[["text", "NORM"]]
+    df['text'] = df['text'].astype(str)
     # flatten the NORM column
     norm_df = pd.json_normalize(df['NORM'])
+    norm_df = norm_df.astype(int)
     df = pd.concat([df, norm_df], axis=1)
     df = df.drop(columns=["NORM"])
 
